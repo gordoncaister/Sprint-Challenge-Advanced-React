2a5622ae992f9f0632da481ebbccb7f919ba100d
Gordon Caister <gordoncaister@gmail.com>
2020-04-10 10:43:43 -0500

    data fetcher class working, state is being passed to datacard

--- client/src/Components/DataFetcher.js
+++ client/src/Components/DataFetcher.js
@@ -1,20 +1,24 @@
 import React from "react"
 import Axios from "axios";
+import {DataCard} from "./DataCard"
+
 class DataFetcher extends React.Component{
     constructor(){
         super();
-        this.state = {}
+        this.state = {data: []}
     }
     componentDidMount(){
         Axios.get("http://localhost:5000/api/players")
         .then(res => {
             console.log(res)
+            this.setState({data: res.data});
         })
     }
 
     render(){
+        console.log("State: ", this.state.data)
         return (
-            <>Hellolol</>
+            <DataCard data={this.state.data} />
         )
     }
 }
