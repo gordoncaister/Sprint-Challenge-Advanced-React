4f45dbdbb8aa471104d013e064db86dd692c13b4
Gordon Caister <gordoncaister@gmail.com>
2020-04-10 12:47:53 -0500

    finished MVP

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, getByText, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("renders App without crashing", () => {
+  render(<App />);
 });
+
+test("Switch theme button works", ()=> {
+ 
+  const {getByText} = render(<App />)
+    const theme = getByText(/switch/i)
+    fireEvent.click(theme);
+
+
+})
