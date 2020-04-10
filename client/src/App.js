32ac462d29065c17699749599ba57db7e2ce14e4
Gordon Caister <gordoncaister@gmail.com>
2020-04-10 11:32:46 -0500

    created custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import DataFetcher from "./Components/DataFetcher"
-
+import {useColours} from "./Hooks/useColours"
 
 import './App.css';
 
 function App() {
+  const {theme, toggleTheme} = useColours();
   return (
-    <div className="App">
-      Hello
+    <div 
+    style={{
+      background: theme === 'dark' ? '#000' : '#fff',
+      color: theme === 'dark' ? '#fff' : '#000',
+    }}
+    className="App"
+    >
+      <h1>List of Women's World Cup Ranked by Interest</h1>
+      <button type="button" onClick={toggleTheme}>
+					Switch theme
+			</button>
       <DataFetcher  />
     </div>
   );
